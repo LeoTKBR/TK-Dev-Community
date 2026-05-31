b05cef60b60c8b46cd8f99674c2f50a1e938442c
LeoTK <leonardo.fabian@hotmail.com>
2026-05-31 14:20:11 -0300

    fix: monster_dummy

---
+++ monster_dummy/monster/monsterdummy.lua
@@ -0,0 +1,115 @@
+local mType = Game.createMonsterType("Monster Dummy")
+local monster = {}
+
+monster.description = "a monster dummy"
+monster.experience = 0
+monster.outfit = {
+    lookTypeEx = 24228
+}
+
+monster.raceId = 35
+monster.Bestiary = {
+	class = "Undead",
+	race = BESTY_RACE_DEMON,
+	toKill = 2500,
+	FirstUnlock = 100,
+	SecondUnlock = 1000,
+	CharmsPoints = 50,
+	Stars = 4,
+	Occurrence = 0,
+	Locations = "Summon, Card, Spell, Command, Etc..."
+	}
+
+monster.health = 100000
+monster.maxHealth = 100000
+monster.race = "fire"
+monster.corpse = 0
+monster.speed = 0
+monster.manaCost = 0
+
+monster.changeTarget = {
+	interval = 4000,
+	chance = 20
+}
+
+monster.strategiesTarget = {
+	nearest = 70,
+	health = 10,
+	damage = 10,
+	random = 10,
+}
+
+monster.flags = {
+	summonable = false,
+	attackable = false,
+	hostile = false,
+	convinceable = false,
+	pushable = false,
+	rewardBoss = false,
+	illusionable = false,
+	canPushItems = true,
+	canPushCreatures = false,
+	staticAttackChance = 70,
+	targetDistance = 1,
+	runHealth = 0,
+	healthHidden = true,
+	isBlockable = false,
+	canWalkOnEnergy = true,
+	canWalkOnFire = true,
+	canWalkOnPoison = true
+}
+
+monster.light = {
+	level = 0,
+	color = 0
+}
+
+monster.summon = {
+	maxSummons = 1,
+	summons = {
+--		{name = "fire elemental", chance = 10, interval = 2000, count = 1}
+	}
+}
+
+monster.voices = {
+	interval = 5000,
+	chance = 10,
+	{text = "Your soul will be mine!", yell = false},
+	{text = "MUHAHAHAHA!", yell = false},
+	{text = "CHAMEK ATH UTHUL ARAK!", yell = false},
+	{text = "I SMELL FEEEEAAAAAR!", yell = false},
+	{text = "Your resistance is futile!", yell = false}
+}
+
+monster.loot = {
+}
+
+monster.attacks = {
+}
+
+monster.defenses = {
+	defense = 100,
+	armor = 100,
+}
+
+monster.elements = {
+	{type = COMBAT_PHYSICALDAMAGE, percent = 100},
+	{type = COMBAT_ENERGYDAMAGE, percent = 100},
+	{type = COMBAT_EARTHDAMAGE, percent = 100},
+	{type = COMBAT_FIREDAMAGE, percent = 100},
+	{type = COMBAT_LIFEDRAIN, percent = 100},
+	{type = COMBAT_MANADRAIN, percent = 100},
+	{type = COMBAT_DROWNDAMAGE, percent = 100},
+	{type = COMBAT_ICEDAMAGE, percent = 100},
+	{type = COMBAT_HOLYDAMAGE , percent = 100},
+	{type = COMBAT_DEATHDAMAGE , percent = 100}
+}
+
+monster.immunities = {
+	{type = "paralyze", condition = true},
+	{type = "outfit", condition = false},
+	{type = "invisible", condition = true},
+	{type = "bleed", condition = false}
+}
+
+mType:register(monster)
